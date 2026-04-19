a29d96c0d71f01bb7eba688ec6d652dbba0a13cd
Superdooperman <51303617+Superdooperman@users.noreply.github.com>
2026-04-18 19:12:47 -0500

    Create service-worker.js

---
+++ pwas/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'pwas-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/pwas/',
+        '/pwas/index.html',
+        '/pwas/TimeTracker.html',
+        '/pwas/Video-downloader.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
